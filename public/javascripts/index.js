efcd3bf8505af4abbf65edeb8b6832cd1ec02fee
bitnami <bitnami@ubuntu.defaultdomain>
2016-10-21 18:26:48 +0000

    Hacer el modificar

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,21 +1,154 @@
-function alumnoPut(){
-    var datos = {
-        "_id" : $("#_id").val(),
-        "id" : $("#id").val(),
-        "nombre" : $("#nombre").val(),
-        "apellido" : $("#apellido").val(),
-        "email" : $("#email").val()      
-    };
+$(document).ready(function () {
+    mainMenu();
+});
 
+function mainMenu() {
+    var text ='<div class="jumbotron"><h2>Académica</h2></div>';
+    $('#main').html(text);
+}
+
+function profesorRead(del) {
     $.ajax({
-        url : '/AlumnoUpdate',
-        type : 'PUT',
-        data : datos,
+        url: '/ProfesorRead',
+        type: 'GET',
         dataType: 'json',
-        success : function(json) {
-            $('<h1/>').text(json.title).appendTo('body');
-            $('<div class="content"/>')
-                .html(json.html).appendTo('body');
+        success: function (json) {
+            //console.log(JSON.stringify(json));
+            $('#main').html('<h3>Profesores</h3>');
+            var table = $('<table />').addClass('table');
+            if (del) {
+                table.append($('<tr />').append('<th>id</th>', '<th>nombre</th>', '<th>apellidos</th>', '<th>email</th>', '<th> </th>'));
+            } else {
+                table.append($('<tr />').append('<th>id</th>', '<th>nombre</th>', '<th>apellidos</th>', '<th>email</th>'));
+            }
+            for (var profe in json) {
+                //console.log(json);
+                if (del === 1) {
+                    table.append($('<tr />').append('<td>' + json[profe].id + '</td>',
+                        '<td>' + json[profe].nombre + '</td>', '<td>' + json[profe].apellido + '</td>',
+                        '<td>' + json[profe].email + '</td>', 
+                        '<td> <button class="btn btn-danger" onclick="profesorDel()" value='+json[profe]._id+' id="_id">eliminar</button></td>'));
+                } else if (del === 2) {
+                    table.append($('<tr />').append('<td>' + json[profe].id + '</td>',
+                        '<td>' + json[profe].nombre + '</td>', '<td>' + json[profe].apellido + '</td>',
+                        '<td>' + json[profe].email + '</td>', 
+                        '<td> <button class="btn btn-sucess" onclick="profesorUpdateForm()" value='+json[profe]._id+' id="_id">modificar</button></td>'));
+                } else {
+                    table.append($('<tr />').append('<td>' + json[profe].id + '</td>',
+                        '<td>' + json[profe].nombre + '</td>', '<td>' + json[profe].apellido + '</td>',
+                        '<td>' + json[profe].email + '</td>'));
+                }
+            }
+            $('#main').append(table);
+        },
+        error: function (xhr, status) {
+            alert('Disculpe, existió un problema');
         }
     });
+}
+
+function profesorForm() {
+    $('#main').html('<h3>Crear nuevo profesor:</h3>');
+    var form = $('<form />').addClass('table');
+    var div = $('<div />').addClass('form-group');
+    div.append('<label for="id">ID:</label>');
+    div.append('<input id="id" type="number" class="form-control" />');
+    form.append(div);
+    div.append('<label for="nombre">NOMBRE:</label>');
+    div.append('<input id="nombre" type="text" class="form-control" />');
+    form.append(div);
+    div = $('<div />').addClass('form-group');
+    div.append('<label for="apellido">APELLIDOS:</label>');
+    div.append('<input id="apellido" type="text" class="form-control" />');
+    form.append(div);
+    div = $('<div />').addClass('form-group');
+    div.append('<label for="email">EMAIL:</label>');
+    div.append('<input id="email" type="email" class="form-control" />');
+    form.append(div);
+    div = $('<div />').addClass('form-group');
+    div.append('<button onclick="profesorPost()" class="btn btn-success"> Aceptar </div>');
+    form.append(div);
+    
+    $('#main').append(form);
+}
+
+function profesorPost() {
+
+    var datos = {
+        "id": $('#id').val(),
+        "nombre": $('#nombre').val(),
+        "apellido": $('#apellido').val(),
+        "email": $('#email').val()
+        
+    };
+    $.ajax({
+        url: '/ProfesorCreate',
+        data: datos,
+        type: 'POST',
+        dataType: 'json',
+        success: function (json) {
+            profesorRead(0);
+        },
+        error: function (xhr, status) {
+            alert('Disculpe, existió un problema');
+        },
+        complete: function (xhr, status) {
+            //alert('Petición realizada');
+        }
+    });
+}
+
+function profesorDel() {
+
+    var datos = {
+        "_id": $('#_id').val()        
+    };
+    $.ajax({
+        url: '/ProfesorDelete',
+        data: datos,
+        type: 'DELETE',
+        dataType: 'json',
+        success: function (json) {
+            profesorRead(1);
+        },
+        error: function (xhr, status) {
+            alert('Disculpe, existió un problema');
+        },
+        complete: function (xhr, status) {
+            //alert('Petición realizada');
+        }
+    });
+}
+
+function profesorDelete(){
+    profesorRead(1);
+}
+
+function profesorUpdate(){
+    profesorRead(2);
+}
+
+function profesorUpdateForm() {
+    $('#main').html('<h3>Crear nuevo profesor:</h3>');
+    var form = $('<form />').addClass('table');
+    var div = $('<div />').addClass('form-group');
+    div.append('<label for="id">ID:</label>');
+    div.append('<input id="id" type="number" class="form-control" />');
+    form.append(div);
+    div.append('<label for="nombre">NOMBRE:</label>');
+    div.append('<input id="nombre" type="text" class="form-control" />');
+    form.append(div);
+    div = $('<div />').addClass('form-group');
+    div.append('<label for="apellido">APELLIDOS:</label>');
+    div.append('<input id="apellido" type="text" class="form-control" />');
+    form.append(div);
+    div = $('<div />').addClass('form-group');
+    div.append('<label for="email">EMAIL:</label>');
+    div.append('<input id="email" type="email" class="form-control" />');
+    form.append(div);
+    div = $('<div />').addClass('form-group');
+    div.append('<button onclick="profesorPost()" class="btn btn-success"> Aceptar </div>');
+    form.append(div);
+    
+    $('#main').append(form);
 }
--- routes/index.js
+++ routes/index.js
@@ -2,113 +2,19 @@ var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Académica' });
-});
-
-/* GET alumnoRead */
-router.get('/AlumnoRead', function(req, res, next){
-    mongoose.model('Alumno').find({}, function(err, alumnos){
-        if(!err)
-            res.render('alumnoRead', {
-                "listaAlumnos" : alumnos
-            });
-        else
-            res.send('error');
-    });
-});
-
-/* GET asignaturaRead */
-router.get('/AsignaturaRead', function(req, res, next){
-    mongoose.model('Asignatura').find({}, function(err, asignaturas){
-        if(!err)
-            res.render('asignaturaRead', {
-                "listaAsignaturas" : asignaturas
-            });
-        else
-            res.send('error');
-    });
-});
-
 /* GET profesorRead */
-router.get('/ProfesorRead', function(req, res, next){
-    mongoose.model('Profesor').find({}, function(err, profesores){
+router.get('/profesorRead', function(req, res, next){
+    var Profesor = mongoose.model('Profesor');
+    
+    Profesor.find({}, function(err, profesores){
         if(!err)
-            res.render('profesorRead', {
-                "listaProfesores" : profesores
-            });
+            res.json(profesores);
         else
-            res.send('error');
-    });
-});
-
-/* GET alumnoCreate */
-router.get('/AlumnoCreate', function(req, res, next){
-    res.render('alumnoCreate');
-});
-
-/* POST alumnoCreate */
-router.post('/AlumnoCreate', function(req, res, next){
-    var Alumno = mongoose.model('Alumno');
-    var alumno = new Alumno();
-
-    alumno.id = req.body.id;
-    alumno.nombre = req.body.nombre;
-    alumno.apellido = req.body.apellido;
-    alumno.email = req.body.email;
-
-    alumno.save(function(err){
-        if(!err)
-            res.redirect("/");
-        else
-            res.render("error", {
-                "message" : "Error: alumnoUpdate",
-                "error" : {
-                    "status" : "Imposible acceder a la base de datos",
-                    "stack" : "Volcado de pila..."
-                }
-            });
+            res.json({});
     })
 });
 
-/* GET asignaturaCreate */
-router.get('/AsignaturaCreate', function(req, res, next){
-    res.render('asignaturaCreate');
-});
-
-/* POST asignaturaCreate */
-router.post('/AsignaturaCreate', function(req, res, next){
-    var Asignatura = mongoose.model('Asignatura');
-    var asignatura = new Asignatura();
-
-    asignatura.id = req.body.id;
-    asignatura.nombre = req.body.nombre;
-    asignatura.ciclo = req.body.ciclo;
-    asignatura.curso = req.body.curso;
-    asignatura.horas = req.body.horas;
-
-    asignatura.save(function(err){
-        if(!err)
-            res.redirect("/");
-        else 
-            res.render("error", {
-                "message" : "Error: asignaturaCreate",
-                "error" : {
-                    "status" : "Imposible acceder a la base de datos",
-                    "stack" : "Volcado de pila..."
-                }
-            });
-    });
-});
-
-/* GET profesorCreate */
-router.get('/ProfesorCreate', function(req, res, next){
-    res.render('profesorCreate');
-});
-
-/* POST profesorCreate */
-router.post('/ProfesorCreate', function(req, res, next){
+router.post('/ProfesorCreate', function(req, res, next) {
     var Profesor = mongoose.model('Profesor');
     var profesor = new Profesor();
 
@@ -118,62 +24,22 @@ router.post('/ProfesorCreate', function(req, res, next){
     profesor.email = req.body.email;
 
     profesor.save(function(err){
-         if(!err)
-            res.redirect("/");
-        else
-            res.render("error", {
-                "message" : "Error: profesorCreate",
-                "error" : {
-                    "status" : "Imposible acceder a la base de datos",
-                    "stack" : "Volcado de pila..."
-                }
-            });
+        if (err) return handleError(err);
+        res.json({});
+    })
+
+});
+
+
+router.delete('/ProfesorDelete', function (req, res, next) {
+    var Profesor = mongoose.model('Profesor');
+    Profesor.remove({"_id": req.body._id}, function (error, data){
+        if (!error) {
+            res.json({});
+        } else {
+            res.send("Error!!!!");
+        }
     });
 });
-
-/* GET alumnoUpdate */
-router.get('/AlumnoUpdate', function(req, res, next){
-    mongoose.model('Alumno').find({}, function(err, alumnos){
-        if(!err)
-            res.render('/alumnoUpdate', {
-                "listaAlumnos" : alumnos
-            });
-        else
-            res.send('error');
-    });
-});
-
-/* POST alumnoUpdate */
-router.post('/AlumnoUpdate', function(req, res, next){
-    if(req.body._id === 'undefined'){
-        res.redirect('/alumnoUpdate');
-    } else {
-        mongoose.model('Alumno').findById(req.body._id, function(err, data){
-            if(!err)
-                res.render("alumnoUpdateForm", {'alumno' : data});
-            });
-    }
-        
-    
-});
-
-/* PUT alumnoUpdate */
-router.put('/AlumnoUpdate', function(req, res, next){
-    mongoose.model('Alumno').findByIdAndUpdate(req.body._id, 
-    { "id" : req.body.id, "nombre" : req.body.nombre, "apellido" : req.body.apellido, "email" : req.body.email}, 
-    function(err){
-        if(!err)
-            res.redirect("/alumnoUpdate");
-        else
-            res.render("error", {
-                "message" : "Error: alumnoUpdate",
-                "error" : {
-                    "status" : "Imposible acceder a la base de datos",
-                    "stack" : "Volcado de pila..."
-                }
-            });
-    });
-});
-
 
 module.exports = router;
